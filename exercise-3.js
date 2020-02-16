d57e32173a06b1e19ed14a0226f0d50e2b0be1ed
cskusuma14 <48611465+cskusuma14@users.noreply.github.com>
2020-02-16 14:56:52 +0700

    Create exercise-3.js

---
+++ exercise-3.js
@@ -0,0 +1,20 @@
+function cariMedian(arr) {
+    
+    var result = 0;
+    if( arr.length%2 === 0)
+    {
+        result = (arr[arr.length /2] + arr[arr.length/2-1]) /2;
+    }
+    else
+    {
+        result = arr[Math.floor(arr.length/2)];
+    }
+    return result;
+  }
+  
+  // TEST CASES
+  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+  console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+  console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
